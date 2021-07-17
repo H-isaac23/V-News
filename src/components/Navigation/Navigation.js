ee7e43617bfe37d66157a3276a042ee9c8ea0860
H-isaac23 <dev.isaac23@gmail.com>
2021-07-17 14:40:51 +0800

    add navs class to navigators

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,12 +4,12 @@ export default function Navbar() {
   return (
     <div className={styles.navbarContainer}>
       <div className={styles.siteName}>
-        <p>V-News</p>
+        <p className={styles.navs}>V-News</p>
       </div>
       <div className={styles.navigators}>
-        <p className={styles.home}>Home</p>
-        <p className={styles.disclaimer}>Disclaimer</p>
-        <p className={styles.credits}>Credits</p>
+        <p className={`${styles.home} ${styles.navs}`}>Home</p>
+        <p className={`${styles.disclaimer} ${styles.navs}`}>Disclaimer</p>
+        <p className={`${styles.credits} ${styles.navs}`}>Credits</p>
       </div>
     </div>
   );
