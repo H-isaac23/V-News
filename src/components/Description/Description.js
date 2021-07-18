ecc17cc17946f8545231b5d5708c72ff525cac81
H-isaac23 <dev.isaac23@gmail.com>
2021-07-18 21:35:52 +0800

    wrap text in div container

--- src/components/Description/Description.js
+++ src/components/Description/Description.js
@@ -6,12 +6,14 @@ export default function Description() {
     <div className={styles.container}>
       <div className={styles.info}>
         <div className={styles.title}>Holo-Updates</div>
-        <div className={styles.intro}>
-          Welcome to V-Updates, a fan site to bring the latest tweets and
-          streams of various virtual youtubers (a.k.a. V-Tubers).
-        </div>
-        <div className={styles.page_info}>
-          You are currently on the Hololive page on the website.
+        <div className={styles.info_container}>
+          <div className={styles.intro}>
+            Welcome to V-Updates, a fan site to bring the latest tweets and
+            streams of various virtual youtubers (a.k.a. V-Tubers).
+          </div>
+          <div className={styles.page_info}>
+            You are currently on the Hololive page on the website.
+          </div>
         </div>
       </div>
       <div className={styles.image}>
