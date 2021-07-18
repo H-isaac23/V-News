d077f479558280712f04f38e9765345a579e66a6
H-isaac23 <dev.isaac23@gmail.com>
2021-07-18 21:49:15 +0800

    refactor class names

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,13 +3,13 @@ import styles from "./Navigation.module.scss";
 export default function Navbar() {
   return (
     <div className={styles.navbarContainer}>
-      <div className={styles.siteName}>
-        <p className={styles.navs}>V-News</p>
+      <div className={styles.siteNameContainer}>
+        <div className={styles.siteName}>V-News</div>
       </div>
       <div className={styles.navigators}>
-        <p className={`${styles.home} ${styles.navs}`}>Home</p>
-        <p className={`${styles.disclaimer} ${styles.navs}`}>Disclaimer</p>
-        <p className={`${styles.credits} ${styles.navs}`}>Credits</p>
+        <div className={`${styles.home} ${styles.navs}`}>Home</div>
+        <div className={`${styles.disclaimer} ${styles.navs}`}>Disclaimer</div>
+        <div className={`${styles.credits} ${styles.navs}`}>Credits</div>
       </div>
     </div>
   );
