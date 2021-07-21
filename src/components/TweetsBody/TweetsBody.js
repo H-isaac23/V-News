3c2dea04427a6c5e85329b57d319072d22393b8f
H-isaac23 <dev.isaac23@gmail.com>
2021-07-21 18:38:02 +0800

    rename updatesbody to tweetsbody

--- src/components/TweetsBody/TweetsBody.js
+++ src/components/TweetsBody/TweetsBody.js
@@ -1,4 +1,4 @@
-import styles from "./UpdatesBody.module.scss";
+import styles from "./TweetsBody.module.scss";
 
 export default function UpdatesBody() {
   return <div className={styles.main}></div>;
