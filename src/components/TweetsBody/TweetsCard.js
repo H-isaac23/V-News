b9bbac70fc176bde70e3a597dabea2368bda116e
H-isaac23 <dev.isaac23@gmail.com>
2021-07-26 12:07:30 +0800

    add content div

--- src/components/TweetsBody/TweetsCard.js
+++ src/components/TweetsBody/TweetsCard.js
@@ -4,11 +4,21 @@ const TweetCard = (props) => {
   return (
     <article className={styles.tweetContainer}>
       <div className={styles.tweetInfo}>
-        <img src={props.imageSrc} alt="user pfp" />
+        <div className={styles.imageWrapper}>
+          <img src={props.imageSrc} alt="user pfp" />
+        </div>
         <div className={styles.handleDate}>
           <p className="handle">{props.handle}</p>
           <p className="date">{props.date}</p>
         </div>
+      </div>
+      <div className={styles.content}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam quia
+        provident ipsa neque accusantium temporibus dolorem esse ratione,
+        cupiditate deleniti! Possimus, cum reiciendis ab deleniti vitae
+        voluptatem illum quia. Nisi, accusamus ratione! Cupiditate sapiente
+        praesentium accusamus consequatur deserunt nihil exercitationem, maxime
+        alias at dolores quidem, libero consequuntur commodi incidunt quo?
       </div>
     </article>
   );
