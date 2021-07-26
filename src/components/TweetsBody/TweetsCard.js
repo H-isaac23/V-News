438850d8329ffcede1be700bf84b2518f9c7ae16
H-isaac23 <dev.isaac23@gmail.com>
2021-07-26 11:59:13 +0800

    remove unnecessary div

--- src/components/TweetsBody/TweetsCard.js
+++ src/components/TweetsBody/TweetsCard.js
@@ -2,14 +2,12 @@ import styles from "./TweetsCard.module.scss";
 
 const TweetCard = (props) => {
   return (
-    <div className={styles.tweet}>
-      <div className={styles.tweetContainer}>
-        <div className={styles.tweetInfo}>
-          <img src={props.imageSrc} alt="user pfp" />
-          <div className={styles.handleDate}>
-            <p className="handle">{props.handle}</p>
-            <p className="date">{props.date}</p>
-          </div>
+    <div className={styles.tweetContainer}>
+      <div className={styles.tweetInfo}>
+        <img src={props.imageSrc} alt="user pfp" />
+        <div className={styles.handleDate}>
+          <p className="handle">{props.handle}</p>
+          <p className="date">{props.date}</p>
         </div>
       </div>
     </div>
