0b5b7de1c64799cc688f332fcd70bf519ff82f4d
H-isaac23 <dev.isaac23@gmail.com>
2021-07-26 11:57:14 +0800

    finish boilerplate for body

--- src/components/TweetsBody/TweetsBody.js
+++ src/components/TweetsBody/TweetsBody.js
@@ -1,10 +1,17 @@
 import styles from "./TweetsBody.module.scss";
+import TweetCard from "./TweetsCard";
+import user_pfp from "../../public/rushia.jpg";
+
+const date = "July 26, 2021";
+const handle = "@uruharushia";
+
 const TweetsBody = () => {
   return (
     <div className={styles.main}>
       <div className={styles.titleContainer}>
         <div className={styles.sectionTitle}>Recent Tweets</div>
       </div>
+      <TweetCard imageSrc={user_pfp} handle={handle} date={date} />
     </div>
   );
 };
