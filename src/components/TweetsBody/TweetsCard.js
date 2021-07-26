0329625089f65a9e8b896ecb558479c4d9abe4df
H-isaac23 <dev.isaac23@gmail.com>
2021-07-26 11:56:57 +0800

    finish card template

--- src/components/TweetsBody/TweetsCard.js
+++ src/components/TweetsBody/TweetsCard.js
@@ -1,11 +1,19 @@
 import styles from "./TweetsCard.module.scss";
 
-const TweetCard = () => {
+const TweetCard = (props) => {
   return (
     <div className={styles.tweet}>
       <div className={styles.tweetContainer}>
-        <div className={styles.tweetInfo}></div>
+        <div className={styles.tweetInfo}>
+          <img src={props.imageSrc} alt="user pfp" />
+          <div className={styles.handleDate}>
+            <p className="handle">{props.handle}</p>
+            <p className="date">{props.date}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
 };
+
+export default TweetCard;
