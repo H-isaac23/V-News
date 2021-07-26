11ff02b942abed8505d3f51619e2aa7be2151322
H-isaac23 <dev.isaac23@gmail.com>
2021-07-26 11:43:47 +0800

    add test content for tweet card

--- src/components/TweetsBody/TweetsCard.js
+++ src/components/TweetsBody/TweetsCard.js
@@ -1,1 +1,11 @@
 import styles from "./TweetsCard.module.scss";
+
+const TweetCard = () => {
+  return (
+    <div className={styles.tweet}>
+      <div className={styles.tweetContainer}>
+        <div className={styles.tweetInfo}></div>
+      </div>
+    </div>
+  );
+};
