8591efac9aea5e891fa5898a98d399e921eae406
H-isaac23 <dev.isaac23@gmail.com>
2021-07-30 15:13:45 +0800

    use react player to embed videos

--- src/components/StreamsBody/VideoCard.js
+++ src/components/StreamsBody/VideoCard.js
@@ -1,1 +1,20 @@
+import ReactPlayer from "react-player";
 import styles from "./VideoCard.module.scss";
+
+const sampleUrl = "https://www.youtube.com/watch?v=XwdR6yGVY1k&pp=sAQA";
+
+const VideoCards = () => {
+  return (
+    <div className={styles.playerWrapper}>
+      <ReactPlayer
+        id={styles.player}
+        url={sampleUrl}
+        height={"100%"}
+        width={"100%"}
+      />
+      ;
+    </div>
+  );
+};
+
+export default VideoCards;
