d944e61c4f427a15e54374d795277a43cecc731b
H-isaac23 <dev.isaac23@gmail.com>
2021-07-30 14:36:43 +0800

    add section title

--- src/components/StreamsBody/StreamsBody.js
+++ src/components/StreamsBody/StreamsBody.js
@@ -1,7 +1,13 @@
 import styles from "./StreamsBody.module.scss";
 
 const StreamBody = () => {
-  return <></>;
+  return (
+    <section className={styles.streams}>
+      <div className={styles.titleContainer}>
+        <div className={styles.sectionTitle}>Recent Streams</div>
+      </div>
+    </section>
+  );
 };
 
 export default StreamBody;
