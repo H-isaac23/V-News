621b4891ec6f8c103995cf14b29946833f13bcf5
H-isaac23 <dev.isaac23@gmail.com>
2021-08-06 23:02:34 +0800

    add more props

--- src/components/StreamsBody/VideoCarousel.js
+++ src/components/StreamsBody/VideoCarousel.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import "../../../node_modules/react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from "react-responsive-carousel";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 import VideoCards from "./VideoCard";
 import styles from "./VideoCarousel.module.scss";
 
@@ -12,11 +12,35 @@ const sampleVideos = [
   "https://www.youtube.com/watch?v=rG0mRyPLQ6A",
 ];
 
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 2,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 2,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
+
 class VideoCarousel extends Component {
   render() {
     return (
       <div className={styles.container}>
-        <Carousel>
+        <Carousel
+          responsive={responsive}
+          infinite
+          itemClass="carousel-item-padding-40-px"
+        >
           {sampleVideos.map((url) => (
             <VideoCards url={url} />
           ))}
