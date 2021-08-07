d9c5353d3828f35b39e939895597d1cdadaefecc
H-isaac23 <dev.isaac23@gmail.com>
2021-08-07 22:15:10 +0800

    make VideoCarousel an arrow func

--- src/components/StreamsBody/VideoCarousel.js
+++ src/components/StreamsBody/VideoCarousel.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import VideoCards from "./VideoCard";
@@ -13,41 +13,22 @@ const sampleVideos = [
 ];
 
 const responsive = {
-  superLargeDesktop: {
-    // the naming can be any, depends on you.
-    breakpoint: { max: 4000, min: 3000 },
-    items: 2,
-  },
   desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 2,
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 2,
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1,
+    breakpoint: { max: 3000, min: 0 },
+    items: 3,
   },
 };
 
-class VideoCarousel extends Component {
-  render() {
-    return (
-      <div className={styles.container}>
-        <Carousel
-          responsive={responsive}
-          infinite
-          itemClass="carousel-item-padding-40-px"
-        >
-          {sampleVideos.map((url) => (
-            <VideoCards url={url} />
-          ))}
-        </Carousel>
-      </div>
-    );
-  }
-}
+const VideoCarousel = () => {
+  return (
+    <div className={styles.container}>
+      <Carousel responsive={responsive} infinite>
+        {sampleVideos.map((url) => (
+          <VideoCards url={url} />
+        ))}
+      </Carousel>
+    </div>
+  );
+};
 
 export default VideoCarousel;
