67fc07ee3cf5b0d9401cac0338bf6c6508ed4e36
H-isaac23 <dev.isaac23@gmail.com>
2021-08-24 22:41:55 +0800

    limit search api to 3 hours prior current time

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,11 @@ Account.find({}).then((res) => {
 const BEARER = `Bearer ${token}`;
 
 const getNewTweets = (acc_id) => {
-  const API_URL = `https://api.twitter.com/2/users/${acc_id}/tweets?max_results=5&expansions=author_id&tweet.fields=public_metrics,text,created_at&user.fields=id,username,name,profile_image_url&exclude=retweets,replies`;
+  let start_time = new Date();
+
+  const API_URL = `https://api.twitter.com/2/users/${acc_id}/tweets?max_results=5&start_time=${new Date(
+    start_time.setTime(start_time.getTime() - 3 * 60 * 60 * 1000)
+  ).toISOString()}&expansions=author_id&tweet.fields=public_metrics,text,created_at&user.fields=id,username,name,profile_image_url&exclude=retweets,replies`;
 
   return axios.get(API_URL, {
     headers: {
