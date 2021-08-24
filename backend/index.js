65c7ca109f1b6ed0c822a96a7d54869a804ae695
H-isaac23 <dev.isaac23@gmail.com>
2021-08-24 18:16:01 +0800

    fix incorrect date creation

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ Account.find({}).then((res) => {
 const BEARER = `Bearer ${token}`;
 
 const getNewTweets = (acc_id) => {
-  const API_URL = `https://api.twitter.com/2/users/${acc_id}/tweets?max_results=5&expansions=author_id&tweet.fields=public_metrics,text&user.fields=id,username,name,profile_image_url&exclude=retweets,replies`;
+  const API_URL = `https://api.twitter.com/2/users/${acc_id}/tweets?max_results=5&expansions=author_id&tweet.fields=public_metrics,text,created_at&user.fields=id,username,name,profile_image_url&exclude=retweets,replies`;
 
   return axios.get(API_URL, {
     headers: {
@@ -43,13 +43,12 @@ app.post("/api/tweets", (req, res) => {
         const data = result.data;
         const tweets = data.data;
         const user_info = data.includes.users;
-        console.log(user_info);
 
         // for all the tweets on an account, make a new Tweet instance and save to db
         tweets.forEach((tweet) => {
           const newTweet = new Tweet({
             handle: user_info[0].username,
-            date: new Date(),
+            date: tweet.created_at,
             content: tweet.text,
             tweet_id: tweet.id,
             profile_image_url: user_info[0].profile_image_url.replace(
@@ -60,8 +59,13 @@ app.post("/api/tweets", (req, res) => {
 
           newTweet
             .save()
-            .then(res.status(200).end())
-            .catch((err) => console.log(err.message)); // in case the tweet already exists in the db
+            .then((result) => {
+              res.status(200).end();
+            })
+            .catch((err) => {
+              res.status(400).end();
+              console.log(err.message);
+            }); // in case the tweet already exists in the db
         });
       })
       .catch((err) => console.log(err));
