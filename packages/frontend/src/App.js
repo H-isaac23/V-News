460bbf9e9ee2747c2288145efeb308bea018a988
H-isaac23 <dev.isaac23@gmail.com>
2021-10-08 22:44:55 +0800

    use react router

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -4,14 +4,23 @@ import Description from "./components/Description/Description";
 import TweetsBody from "./components/TweetsBody/TweetsBody";
 import StreamsBody from "./components/StreamsBody/StreamsBody";
 import Footer from "./components/Footer/Footer";
+import Disclaimer from "./components/Disclaimer/Disclaimer";
+import { Switch, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <Description />
-      <TweetsBody />
-      <StreamsBody />
+      <Switch>
+        <Route path="/disclaimer">
+          <Disclaimer />
+        </Route>
+        <Route path="/">
+          <Description />
+          <TweetsBody />
+          <StreamsBody />
+        </Route>
+      </Switch>
       <Footer />
     </div>
   );
