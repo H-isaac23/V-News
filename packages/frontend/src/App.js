5eb62dd84e3393f90c6b4902c0db4307f6512dd3
H-isaac23 <dev.isaac23@gmail.com>
2021-10-08 23:03:05 +0800

    add style for footer at bottom

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -6,21 +6,24 @@ import StreamsBody from "./components/StreamsBody/StreamsBody";
 import Footer from "./components/Footer/Footer";
 import Disclaimer from "./components/Disclaimer/Disclaimer";
 import { Switch, Route } from "react-router-dom";
+import styles from "./App.module.scss";
 
 const App = () => {
   return (
-    <div>
+    <div className={styles.appContainer}>
       <Navbar />
-      <Switch>
-        <Route path="/disclaimer">
-          <Disclaimer />
-        </Route>
-        <Route path="/">
-          <Description />
-          <TweetsBody />
-          <StreamsBody />
-        </Route>
-      </Switch>
+      <div className={styles.mainContent}>
+        <Switch>
+          <Route path="/disclaimer">
+            <Disclaimer />
+          </Route>
+          <Route path="/">
+            <Description />
+            <TweetsBody />
+            <StreamsBody />
+          </Route>
+        </Switch>
+      </div>
       <Footer />
     </div>
   );
