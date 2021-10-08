bae49c408f49c882136dc8d2e51f87e378b92e3b
H-isaac23 <dev.isaac23@gmail.com>
2021-10-08 22:43:48 +0800

    init disclaimer component

---
+++ packages/frontend/src/components/Disclaimer/Disclaimer.js
@@ -0,0 +1,11 @@
+import styles from "./Disclaimer.module.scss";
+
+const Disclaimer = () => {
+  return (
+    <div className={styles.disclaimerWrapper}>
+      <h3>Disclaimer</h3>
+    </div>
+  );
+};
+
+export default Disclaimer;
